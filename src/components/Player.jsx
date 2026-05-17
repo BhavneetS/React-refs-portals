ad618b3a2325560bd9f1cc3f2777d68b7d0dd900
BhavneetS <bhavneet.singh@hotmail.com>
2026-05-17 17:30:22 +0530

    Refactoring code using Ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,28 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
+
+  console.log("component is re-rendered")
+  const playerNameRef = useRef();
   
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
+  // const [submitted, setSubmitted] = useState(false);
 
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
+  // function handleChange(event) {
+  //   setSubmitted(false);
+  //   setEnteredPlayerName(event.target.value);
+  // }
 
   function handleClick() {
-    setSubmitted(true);
+    setEnteredPlayerName(playerNameRef.current.value);
+    // setSubmitted(true);
   }
   
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} />
+        <input type="text" ref={playerNameRef} />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
